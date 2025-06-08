757c325e7829de60543f4416c2a9fa8fc784dd60
Chris <144112035+chrisj7903@users.noreply.github.com>
2025-06-08 18:16:55 +0930

    Update ZZ.cpp - add FILTERING & LOAD_AMPS
    added bool FILTERING
    added bool LOAD_AMPS
    add FILTERING to processSerialCommands()

--- SolarController/ZZ.cpp
+++ SolarController/ZZ.cpp
@@ -2,9 +2,14 @@
 
 #include "ZZ.h"
 
-#include <ctype.h>                    // provides toupper() function
+#include <ctype.h>                                            // provides toupper() function
 
-bool VERBOSE  = false;  // true = verbose,            false = quiet mode
+bool VERBOSE  = false;                                        // true = verbose,            false = quiet mode
+bool FILTERING = false;                                       // true = filtering on, false = off 
+// Some Victon SC do not support load amps (e.g. MPPT100/30)
+// However others do (e.g SmartSolar MPPT 75/10,75/15,100/15 & 100/20)   
+// To disable load amps reporting, set to false
+bool LOAD_AMPS = true;                                        // true = Enabled, false = Disabled 
 
 const char dashes[] PROGMEM = " ------------------- ";
 const char line[]   PROGMEM = "..........................................................\n";
@@ -18,6 +23,8 @@ void processSerialCommands() {
       switch(readChar){
         case 'V': if (VERBOSE)  {VERBOSE  = false; Serial << F("\nVERBOSE - off\n\n") ;}
                   else          {VERBOSE  = true;  Serial << F("\nVERBOSE - ON\n")    ;} break;
+        case 'F': if (FILTERING){FILTERING = false; Serial << F("\nFILTERING - off\n\n");}
+                  else          {FILTERING = true;  Serial << F("\nFILTERING - ON\n\n" );} break;        
       } 
     } 
   } 
